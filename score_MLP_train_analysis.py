8f34e6dc24e7df15c341cf502e21d1de377a706e
Binxu Wang <binxu_wang@hms.harvard.edu>
2024-01-04 12:36:25 -0500

    add analysis online

--- score_MLP_train_analysis.py
+++ score_MLP_train_analysis.py
@@ -3,12 +3,13 @@ import pickle as pkl
 import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join
-#%%
-
-rootdir = r"/Users/binxuwang/DL_Projects/HaimDiffusionRNNProj/Shape2d_MLP_train_kempner"
-expdir = join(rootdir, "spiral_20")
-# list pkl files
 import glob
+#%%
+rootdir = r"/Users/binxuwang/DL_Projects/HaimDiffusionRNNProj/Shape2d_MLP_train_kempner"
+rootdir = r"/n/holylabs/LABS/kempner_fellows/Users/binxuwang/DL_Projects/HaimDiffusionRNNProj/Shape2d_MLP_train_kempner"
+dataset_str = "ring_20"
+expdir = join(rootdir, f"{dataset_str}_lr_scaling")
+# list pkl files
 pkl_list = glob.glob(join(expdir, "*.pkl"))
 
 # %%
@@ -16,11 +17,11 @@ pkl_list
 # %%
 """Fixed width, vary depth"""
 # mlp_depth = 3
-mlp_width = 64
+mlp_width = 256
 temb_dim = 32
 plt.figure()
 for mlp_depth in [2, 3, 4, 6, 8]:
-    filename = f"spiral_20_NN_train_temb{temb_dim}_depth{mlp_depth}_width{mlp_width}_batch2048_ep5000.pkl"
+    filename = f"{dataset_str}_NN_train_temb{temb_dim}_depth{mlp_depth}_width{mlp_width}_batch2048_ep5001.pkl"
     try:
         data = pkl.load(open(join(expdir, filename), "rb"))
         meta, loss_traj = data
@@ -38,8 +39,8 @@ mlp_depth = 3
 # mlp_width = 64
 temb_dim = 32
 plt.figure()
-for mlp_width in [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]:
-    filename = f"spiral_20_NN_train_temb{temb_dim}_depth{mlp_depth}_width{mlp_width}_batch2048_ep5000.pkl"
+for mlp_width in [8, 16, 32, 64, 128, 256, 512, 1024, ]:
+    filename = f"{dataset_str}_NN_train_temb{temb_dim}_depth{mlp_depth}_width{mlp_width}_batch2048_ep5001.pkl"
     try:
         data = pkl.load(open(join(expdir, filename), "rb"))
         meta, loss_traj = data
@@ -50,17 +51,38 @@ for mlp_width in [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]:
     plt.plot(loss_traj_smooth, label=f"width={mlp_width}")
 plt.title(f"Smooth loss traj\nMLP depth={mlp_depth}, temb_dim={temb_dim}")
 plt.legend()
-plt.ylim([0.7, 1.1])
+# plt.ylim([0.7, 1.1])
+plt.show()
+
+# %%
+"""Fixed depth, vary width"""
+mlp_depth = 3
+# mlp_width = 64
+temb_dim = 32
+plt.figure()
+for mlp_width in [8, 16, 32, 64, 128, 256, 512, 1024, ]:
+    filename = f"{dataset_str}_NN_train_temb{temb_dim}_depth{mlp_depth}_width{mlp_width}_batch2048_ep5001.pkl"
+    try:
+        data = pkl.load(open(join(expdir, filename), "rb"))
+        meta, loss_traj = data
+    except FileNotFoundError:
+        continue
+    # smooth loss
+    loss_traj_smooth = np.convolve(loss_traj, np.ones(25)/25, mode="valid")
+    plt.loglog(loss_traj_smooth, label=f"width={mlp_width}")
+plt.title(f"Smooth loss traj\nMLP depth={mlp_depth}, temb_dim={temb_dim}")
+plt.legend()
+# plt.ylim([0.7, 1.1])
 plt.show()
 
 #%%
-"""Fixed depth, vary width"""
+"""Fixed depth, width, vary temb_dim"""
 mlp_depth = 4
-mlp_width = 128
+mlp_width = 32
 temb_dim = 32
 plt.figure()
 for temb_dim in [16, 32, 64, 128,]:
-    filename = f"spiral_20_NN_train_temb{temb_dim}_depth{mlp_depth}_width{mlp_width}_batch2048_ep5000.pkl"
+    filename = f"{dataset_str}_NN_train_temb{temb_dim}_depth{mlp_depth}_width{mlp_width}_batch2048_ep5001.pkl"
     try:
         data = pkl.load(open(join(expdir, filename), "rb"))
         meta, loss_traj = data
@@ -71,11 +93,8 @@ for temb_dim in [16, 32, 64, 128,]:
     plt.plot(loss_traj_smooth, label=f"t emb dim={temb_dim}")
 plt.title(f"Smooth loss traj\nMLP depth={mlp_depth}, width={mlp_width}")
 plt.legend()
-plt.ylim([0.7, 1.1])
+# plt.ylim([0.7, 1.1])
 plt.show()
-#%%
-0.005 * 256 /(8+32)
-
 
 # %%
 import torch
